73408fbc0bdc0642e093d0f4d1a204d67a562116
Richie <you@example.com>
2026-04-04 18:31:05 +0100

    change next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  // Explicitly disable App Router
-  experimental: {
-    appDir: false,
-  },
 }
 
 module.exports = nextConfig
